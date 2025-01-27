4839c38329bbc090b03665b9e57b59b0195239d5
x0dyR <manoshki.dan4ik@gmail.com>
2025-01-27 06:36:32 +0500

    added photos

---
+++ src/main.js
@@ -0,0 +1,36 @@
+const path = require(`path`)
+const { app, BrowserWindow, Menu } = require(`electron`)
+
+function createMainWindow() {
+    const mainWindow = new BrowserWindow({
+        title: `Seriy gey`,
+        width: 1920,
+        height: 720
+    });
+
+    mainWindow.loadFile(path.join(__dirname, "index.html"))
+}
+
+app.whenReady().then(() => {
+    createMainWindow();
+
+    const mainMenu = Menu.buildFromTemplate(menu);
+    Menu.setApplicationMenu(mainMenu);
+
+    app.on(`activate`, () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createMainWindow();
+        }
+    });
+});
+
+const menu = [{
+    label: `File`,
+    submenu: [
+        {
+            lable: `Quit`,
+            click: () => app.quit(),
+            accelerator: `CmdOrCtrl+W`
+        }
+    ]
+}];
