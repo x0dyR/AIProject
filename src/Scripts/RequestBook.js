5b4a67a3a25485e39bb3363935842f6ba0a09d5e
x0dyR <87077140305d@gmail.com>
2025-01-28 17:47:35 +0500

    .

--- src/Scripts/RequestBook.js
+++ src/Scripts/RequestBook.js
@@ -1,1 +1,49 @@
-console.log(document.getElementsByClassName(`BookRequestText`).value);
+const { } = require(`dotenv`).config();
+const { OpenAI } = require(`openai`)
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_KEY,
+});
+
+const completion = openai.chat.completions.create({
+    model: "gpt-4o-mini",
+    store: true,
+    messages: [
+        { "role": "user", "content": "write a haiku about ai" },
+    ],
+});
+
+completion.then((result) => console.log(result.choices[0].message));
+// const { OpenAI } = require(`openai`)
+
+// const openai = new OpenAI({ apiKey: process.env.OPENAI_KEY });
+
+// function SendRequest() {
+//     document.getElementsByClassName(`SendRequest`)[0].addEventListener(`click`, async () => {
+//         try {
+//             await TestRequest();
+
+//         } catch (error) {
+//             console.log(error)
+//         }
+//     })
+// }
+
+// async function TestRequest() {
+//     const stream = await openai.chat.completions.create({
+//         model: `chatgpt-4o-latest`,
+//         messages: [{ role: `user`, content: `Say this is test` }],
+//         store: true,
+//         stream: true,
+//     });
+
+//     for await (const chunk of stream) {
+//         process.stdout.write(chunk.choices[0]?.delta?.content || ``);
+//     }
+// }
+
+function RequestBookText() {
+    var inputValue = document.getElementsByClassName("BookRequestText")[0];
+    var text = inputValue.value;
+    console.log(text)
+}
